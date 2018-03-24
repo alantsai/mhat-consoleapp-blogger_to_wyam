9a7879dfa9d3f0556864ed73e0f291c77d23d6ab
Alan Tsai <alan@alantsai.net>
2018-03-24 21:15:06 +0800

    deseralize and show count

--- src/MHAT.BloggerToWyam.ConsoleApp/Bll/Process/XmlBackupProcess.cs
+++ src/MHAT.BloggerToWyam.ConsoleApp/Bll/Process/XmlBackupProcess.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 using MHAT.BloggerToWyam.ConsoleApp.Model;
 using MHAT.ConsoleApp.ProcessTemplate;
 
 namespace MHAT.BloggerToWyam.ConsoleApp.Bll.Process
 {
-    public class XmlBackupProcess : BaseReadInputProcessTemplate<XmlBackupProcessOption>
+    public class XmlBackupProcess : BaseExecuteProcessTemplate<XmlBackupProcessOption>
     {
-        protected override void ProcessLine(string line)
+        protected override void Process()
         {
             Console.WriteLine($"處理檔案：{ArugemntOption.BackupXmlPath}");
 
+            XmlSerializer ser = new XmlSerializer(typeof(feed));
+            feed feed;
+            using (XmlReader reader = XmlReader.Create(ArugemntOption.BackupXmlPath))
+            {
+                feed = (feed)ser.Deserialize(reader);
+            }
 
+            Console.WriteLine($"總比數：{feed.entry.Length}");
 
             Console.WriteLine("完成");
         }
