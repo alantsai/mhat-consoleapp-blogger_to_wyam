803b39da726ddf71ba159922443ca9623bc265ae
Alan Tsai <alan@alantsai.net>
2018-03-24 20:58:15 +0800

    add option model

---
+++ src/MHAT.BloggerToWyam.ConsoleApp/Model/XmlBackupProcessOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace MHAT.BloggerToWyam.ConsoleApp.Model
+{
+    class XmlBackupProcessOption
+    {
+        [Option('p', Required = true, HelpText = "匯出的xml路徑")]
+        public string BackupXmlPath { get; set; }
+    }
+}
