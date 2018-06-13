b28df8b3a096e6ac7253b343fe7f10c4e018f7e3
Alan Tsai <alan@alantsai.net>
2018-06-13 14:26:23 +0800

    把匯出的xml加入到專案裡面 - 調整輸入輸出參數方便執行

--- src/MHAT.BloggerToWyam.ConsoleApp/Model/XmlBackupProcessOption.cs
+++ src/MHAT.BloggerToWyam.ConsoleApp/Model/XmlBackupProcessOption.cs
@@ -9,13 +9,13 @@ namespace MHAT.BloggerToWyam.ConsoleApp.Model
 {
     public class XmlBackupProcessOption
     {
-        [Option('p', HelpText = "匯出的xml路徑", DefaultValue = @"d:\Library\Downloads\blog-03-22-2018.xml")]
+        [Option('p', HelpText = "匯出的xml路徑", DefaultValue = @".\Assets\blog-03-22-2018.xml")]
         public string BackupXmlPath { get; set; }
 
         [Option('i', HelpText = "已經下載的圖片位置", DefaultValue = @"d:\Library\Downloads\blog\image\")]
         public string OffLineImagePath { get; set; }
 
-        [Option('o', HelpText = "最後輸出的位置", DefaultValue = @"d:\Library\Downloads\blog\post\")]
+        [Option('o', HelpText = "最後輸出的位置", DefaultValue = @".\post\")]
         public string FinalOutputPostPath { get; set; }
     }
 }
